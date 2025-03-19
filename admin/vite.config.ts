711749a9163e2ad2223931e2916764f5ccf7569c
codepzj <email@codepzj.cn>
2025-03-20 02:20:38 +0800

    chore: 更新依赖

--- admin/vite.config.ts
+++ admin/vite.config.ts
@@ -2,12 +2,14 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import Components from "unplugin-vue-components/vite";
 import { AntDesignVueResolver } from "unplugin-vue-components/resolvers";
+import tailwindcss from "@tailwindcss/vite";
 import { resolve } from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    tailwindcss(),
     Components({
       resolvers: [
         AntDesignVueResolver({
