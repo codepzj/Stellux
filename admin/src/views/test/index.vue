8a4b48668f8df5245728d66a4ffa4904409566eb
codepzj <email@codepzj.cn>
2025-03-29 14:17:54 +0800

    feat: 添加uppy支持

--- admin/src/views/test/index.vue
+++ admin/src/views/test/index.vue
@@ -1,50 +1,29 @@
-<template>
-  <a-table :columns="columns" :data-source="data" :scroll="{ x: 2000 }" :expand-column-width="100">
-    <template #bodyCell="{ column }">
-      <template v-if="column.key === 'action'">
-        <a>Delete</a>
-      </template>
-    </template>
-    <template #expandedRowRender="{ record }">
-      <p style="margin: 0">
-        {{ record.description }}
-      </p>
-    </template>
-    <template #expandColumnTitle>
-      <span style="color: red">More</span>
-    </template>
-  </a-table>
-</template>
-<script lang="ts" setup>
-const columns = [
-  { title: 'Name', dataIndex: 'name', key: 'name', fixed: true },
-  { title: 'Age', dataIndex: 'age', key: 'age' },
-  { title: 'Address', dataIndex: 'address', key: 'address' },
-  { title: 'Action', key: 'action' },
-];
+<script setup lang="ts">
+import Dashboard from "@uppy/dashboard";
+import ImageEditor from "@uppy/image-editor";
+import Uppy from "@uppy/core";
+import zh_CN from "@uppy/locales/lib/zh_CN";
+import XHR from "@uppy/xhr-upload";
 
-const data = [
-  {
-    key: 1,
-    name: 'John Brown',
-    age: 32,
-    address: 'New York No. 1 Lake Park',
-    description: 'My name is John Brown, I am 32 years old, living in New York No. 1 Lake Park.',
-  },
-  {
-    key: 2,
-    name: 'Jim Green',
-    age: 42,
-    address: 'London No. 1 Lake Park',
-    description: 'My name is Jim Green, I am 42 years old, living in London No. 1 Lake Park.',
-  },
-  {
-    key: 3,
-    name: 'Joe Black',
-    age: 32,
-    address: 'Sidney No. 1 Lake Park',
-    description: 'My name is Joe Black, I am 32 years old, living in Sidney No. 1 Lake Park.',
-  },
-];
+import "@uppy/core/dist/style.css";
+import "@uppy/dashboard/dist/style.css";
+import "@uppy/image-editor/dist/style.min.css";
+
+import { onMounted } from "vue";
+
+onMounted(() => {
+  new Uppy({
+    locale: zh_CN,
+  })
+    .use(Dashboard, { inline: true, target: "#uppy-dashboard" })
+    .use(ImageEditor)
+    .use(XHR, { endpoint: "https://your-domain.com/upload" })
+    .on("complete", result => {
+      console.log("Upload result:", result);
+    });
+});
 </script>
 
+<template>
+  <div id="uppy-dashboard"></div>
+</template>
